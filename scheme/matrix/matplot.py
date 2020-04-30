4c98ed4541a69233d2bcd257bfac0452e76df23b
james4deutschland <james4deutschland@gmail.com>
2020-04-30 16:34:54 +0800

    fuck

--- scheme/matrix/matplot.py
+++ scheme/matrix/matplot.py
@@ -7,5 +7,5 @@ def getnum(a,b):
 def randomMatrix(a,b,c):
     return np.matrix([[getnum(a,b)+getnum(a,b)*1J for x in range(c)] for x in range(c)])
 
-r=randomMatrix(-10,10,10)
-print(r)
+#r=randomMatrix(-10,10,10)
+#print(r)
