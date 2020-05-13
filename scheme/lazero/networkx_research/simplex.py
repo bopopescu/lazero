daa1e4497768ba885cc94e493920f858f9fa1d4a
yukio233 <yukio233@gitee.com>
2020-05-13 15:12:14 +0800

    fuck

--- scheme/lazero/networkx_research/simplex.py
+++ scheme/lazero/networkx_research/simplex.py
@@ -1,4 +1,6 @@
 import re
+
+
 def openfinder(a):
-    with open(a,"rb") as f:
-        return re.findall(r'[a-zA-Z0-9\-\_]+',str(f.read()))
+    with open(a, "rb") as f:
+        return re.findall(r'[a-zA-Z0-9\-\_]+', str(f.read()))
