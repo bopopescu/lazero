589a4b09eafaa92c05962b2bb3e3e65047a25487
root <root@localhost.localdomain>
2020-06-11 15:26:08 -0500

    upload

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 # if written as one-linear then fault tolerance will get lower
 
 git add . 
-git commit -m "fuck" 
+git commit -m "upload" 
 git push origin master
 # never mind. it is all about practice.
 # you can do this to achieve anything.
