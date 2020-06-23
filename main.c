6d68fe4412aa4871543c42966cbad5a6b6f9f634
James Brown <37745685+James4Deutschland@users.noreply.github.com>
2020-06-23 09:50:15 +0800

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 // #include <sqlite3.h>
 int main(int argc, char* argv[]) {
     printf("this is the main lazero program.\n");
+    // do you consider doing this above some source code?
+    // like chrome or something? I cannot write shit at this place!
+    // curl will also be great. wget, nc....
+    // some basic GNU tools?
     // printf("%d",argc);
     // system("pwd");
     // just how to write a shell???
