71e26f157ddad0587a291ba6a7a28cffa8de6870
root <root@localhost.localdomain>
2020-07-10 17:26:45 -0500

    fuck

--- lazero.sh
+++ lazero.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/fish
 command c main.c | xargs c lazero.c &> /dev/shm/lazero
 # i swear this is pure shit. nothing about grammar tree or nothing. it is something must be admitted for sure.
+# the problem is simple. it is not simple enough.
+# you really should pick up some fundamental shits to read on.
+# whatsoever. it is all due to low quota.
+# yes you push too damn hard on so-called things.
+# you are not good at them. you cannot teach them.
--- upload.sh
+++ upload.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
-# if written as one-linear then fault tolerance will get lower
+# if written as one-linear then fault tolerance will get lower.
 git add . 
 git commit -m "fuck" 
 git push origin master
+# there's no fucking shit about perfect design.
+# nope. there's not.
+# remember you will never be able to write the core on your own.
+# REMEMBER THAT YOU WILL NEVER BE ABLE TO WRITE THE CORE ON YOUR OWN.
+# it is just a platform.
+# just remember it will never gonna win.
+# and this piece of code is far from perfect.
+# fucking data?
+# just consider it.
+# the thing is not right. you'd better let your computer learn it. test if it can run the stuff.
+# you already have it.
+# both static and dynamic.
+# informative and functional.
+# just some verifications.
+# ask your machine to type "I WILL KILL ALL HUMEN"?
+# do not hope to do perfect shit. think only about those things that you can do right now.
 # never mind. it is all about practice.
 # you can do this to achieve anything.
+# but how the fuck does that thing works out?
+# calm down.
+# you do not love files, but these are like books for your machine.
 # you want to build a system crawler? can't crawl from the web?
+# whatever you say. IT IS NOW MY BRAIN NOT WORKING PROPERLY.
+# I NEED TO CHANGE THIS SITUATION.
+# you see, it is all just about learning. learning matters the most.
+# and you have not build a unified agent to learn all these stuffs.
+# check what to imitate. is it about the OPERATE SYSTEM?
