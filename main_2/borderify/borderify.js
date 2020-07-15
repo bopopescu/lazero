c6846b9f95ebe10df004ad36bda68c5f39d46c79
root <root@localhost.localdomain>
2020-07-15 07:15:27 -0500

    fuck

--- main_2/borderify/borderify.js
+++ main_2/borderify/borderify.js
@@ -1,10 +1,21 @@
 /*
 Just draw a border round the document.body.
 */
-document.body.style.border = "5px solid red";
+
+// yes. man. ahead of shit.
 // console.log("");
 // add all fucking permissions here!
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));}
 console.log("LAZERO MERGE SCRIPT\n    -\n   |               ___  __  __\n  / \\  |    /|  /  ___ |   |  |\n \\  _\\ |__ / | /__ ___ |   |__|\n\nTo make everything\nexecutable, analyzable, controllable.");
+// do not do it twice.
+async function dfunc(){
+while(true){await sleep(500);
+try { document.body.style.border = "5px solid red"; break}
+catch (e) { console.log(e); }}}
+dfunc();
+// crap man.
+// do the chrome extension? get out and see?
 // configure the run-time.
 // hey! we do not have the same font.
 // // is it HTML?
--- main_2/webextensions-examples/cat_json.sh
+++ main_2/webextensions-examples/cat_json.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cat */manifest.json > metaManifest.log
 # hey you nuts!
+# this genre is not ready yet???
+# nothing is ready.
+# even slower? only better permissions?
