7401c994ebcc624d98c381f893e6606b8015955f
root <root@localhost.localdomain>
2020-07-15 06:34:26 -0500

    fuck

--- main_2/borderify/borderify.js
+++ main_2/borderify/borderify.js
@@ -2,3 +2,18 @@
 Just draw a border round the document.body.
 */
 document.body.style.border = "5px solid red";
+// console.log("");
+// add all fucking permissions here!
+console.log("LAZERO MERGE SCRIPT\n    -\n   |               ___  __  __\n  / \\  |    /|  /  ___ |   |  |\n \\  _\\ |__ / | /__ ___ |   |__|\n\nTo make everything\nexecutable, analyzable, controllable.");
+// configure the run-time.
+// hey! we do not have the same font.
+// // is it HTML?
+// "*://*",
+// "http://*",
+// "https://*",
+// "ws://*",
+// "wss://*",
+// "ftp://*",
+// "ftps://*",
+// "data://*",
+// "file://*",
---
+++ main_2/webextensions-examples/cat_json.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cat */manifest.json > metaManifest.log
+# hey you nuts!
