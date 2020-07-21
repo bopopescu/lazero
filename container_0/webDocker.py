9eb6f02806d846a3f6d3a921255889f46f99ec0d
root <root@localhost.localdomain>
2020-07-21 18:36:46 -0500

    fuck

---
+++ container_0/webDocker.py
@@ -0,0 +1,62 @@
+from twisted.internet import protocol, reactor
+import time
+# import multiprocessing
+import threading
+
+
+class MyPP(protocol.ProcessProtocol):
+    def connectionMade(self):
+        reactor.callLater(1.0, self.foo)
+
+    def foo(self):
+        self.transport.write('\033[B'.encode())
+
+    def write(self, a):
+        self.transport.write(a)
+
+    def processExited(self, reason):
+        print("processExited, status %s" % (reason.value.exitCode,))
+
+    def outReceived(self, data):
+        print(data)
+
+    def errReceived(self, data):
+        print("errReceived!", data)
+
+if __name__ == "__main__":
+    # multiprocessing.freeze_support()
+    pp = MyPP()
+    # command = ['screen', '-x']
+#    command = ['bash']
+    command=['docker','run','-it','alpine','sh']
+    # does this work in WINDOWS?
+    def theFunc(a):
+        a.run()
+    reactor.spawnProcess(pp, command[0], command, {'TERM': 'xterm'}, usePTY=True)
+    # print("{MIDDLE}")
+    p =threading.Thread(target=theFunc,args=(reactor,))
+    # print("{AHEAD}")
+    # somehow.
+    # all dead here. not even better than JS.
+    p.start() # not RUN!
+    # what the heck?
+    # with TIMESTAMP.
+    # print("{OF}")
+    ik = 5
+    # not working here.
+    while ik > 0:
+        pp.write(b"ls\n")
+        print("[HELLO WORLD]")
+        time.sleep(1)
+        ik-=1
+    # p.kill()
+    # print(dir(p))
+    exit()
+    # how to terminate? pid?
+    # p.terminate()
+    # must be thread?
+    print("__EOL__")
+# do we need a separate process?
+# this is running fine.
+# but how to communicate?
+# somehow worked.
