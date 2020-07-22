c855aba33c057dd52acb2227b08b810d18dc99f3
root <root@localhost.localdomain>
2020-07-22 08:19:10 -0500

    fuck

---
+++ comp.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+n=1+1J
+n0=np.tanh(n)
+print(n0)
---
+++ sample_manager.py
@@ -0,0 +1,33 @@
+# import dogtail
+from dogtail import *
+# actually you can find out what is in here by looking into the globals.
+# can you do anonymous shits? adding prefix will do the trick.
+# finally get it right?
+f="________________________________________________________________"
+# # distribution?
+# # fuck the code?
+# print(f)
+# for x in dir(utils):
+#     print(x)
+r=tree.root
+print(r,type(r))
+print(f)
+for x in dir(r):
+    print(x)
+print(f)
+print(r.dump())
+# this is fucking awesome.
+# what is this class??
+    # always the fucking cron job!
+    # it is hard to tell. check that shit first?
+    # this is gnome compatible. but remember, there's some tinycore linux over the spot.
+    # well, we don't use it.
+    # there is a sniff util.
+# f=utils.isA11yEnabled()
+# f=utils.screenshot()
+# print(f)
+# returning a path.
+# it can be done with a little script.
+# whatever. this is not the main focus.
+# print the tree of all shits? what about windows?
+# shit man.
