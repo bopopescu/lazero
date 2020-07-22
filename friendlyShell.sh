f16378b926b39fc46bd1c1aa3f71c544e2056b96
parrot <parrot@localhost.localdomain>
2020-07-22 14:21:39 -0400

    fuck

--- friendlyShell.sh
+++ friendlyShell.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # firejail --cgroup=/sys/fs/cgroup/lazero/tasks  --apparmor --disable-mnt bash
-firejail --apparmor --disable-mnt bash
+firejail --private --blacklist=/ --read-only=/home --nonewprivs --noroot --apparmor --disable-mnt bash
+# what the heck is going on? why it is able to modify shits?
