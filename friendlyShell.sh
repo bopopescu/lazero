e22d886f189eaeb9137cf10fe62e1690eccfcda3
root <root@localhost.localdomain>
2020-07-23 10:03:45 -0500

    fuck

--- friendlyShell.sh
+++ friendlyShell.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # firejail --cgroup=/sys/fs/cgroup/lazero/tasks  --apparmor --disable-mnt bash
-firejail --private --blacklist=/ --read-only=/home --nonewprivs --noroot --apparmor --disable-mnt bash
+# so what? file size and more?
+firejail --rlimit-as=15000000 --rlimit-cpu=1 --rlimit-fsize=2000000  --private --blacklist=/ --read-only=/home --nonewprivs --noroot --apparmor --disable-mnt bash
 # what the heck is going on? why it is able to modify shits?
